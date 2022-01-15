64ab50591d4a227f3d452e527d6d22c73d3a234a
Alfaa719 <nurul.hidayat.f3@gmail.com>
2022-01-15 10:54:41 +0700

    Make a view login

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export const Login = () => {
+    return (
+        <section className="hero has-background-grey-light is-fullheight is-fullwidth">
+            <div className="hero-body">
+                <div className="container">
+                    <div className="columns is-centered">
+                        <div className="column is-4-desktop">
+                            <form action="" className="box">
+                                <div className="field mt-5">
+                                    <label className="label">Email or Username</label>
+                                    <div className="controls">
+                                        <input type="text" name="" id="" placeholder='Username' className="input" />
+                                    </div>
+                                </div>
+                                <div className="field mt-5">
+                                    <label className="label">Password</label>
+                                    <div className="controls">
+                                        <input type="password" name="" id="" placeholder='******' className="input" />
+                                    </div>
+                                </div>
+                                <div className="field mt-5">
+                                    <button className="button is-success is-fullwidth">Login</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default Login;
