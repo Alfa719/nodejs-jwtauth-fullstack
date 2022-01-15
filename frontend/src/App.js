1195bbcac36fde9e194dca9c8763e20b7806560b
Alfaa719 <nurul.hidayat.f3@gmail.com>
2022-01-15 11:14:49 +0700

    Make a view register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
+import Register from "./components/Register";
+
 
 function App() {
   return (
-    <div>
-      <Login/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/Register.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+export const Register = () => {
+    return (
+        <section className="hero has-background-grey-light is-fullheight is-fullwidth">
+            <div className="hero-body">
+                <div className="container">
+                    <div className="columns is-centered">
+                        <div className="column is-5-desktop">
+                            <form action="" className="box">
+                                <div className="field mt-5">
+                                    <label className="label">Name</label>
+                                    <div className="controls">
+                                        <input type="text" name="" id="" placeholder='Name' className="input" />
+                                    </div>
+                                </div>
+                                <div className="field mt-5">
+                                    <label className="label">Email</label>
+                                    <div className="controls">
+                                        <input type="text" name="" id="" placeholder='Email' className="input" />
+                                    </div>
+                                </div>
+                                <div className="field mt-5">
+                                    <label className="label">Password</label>
+                                    <div className="controls">
+                                        <input type="password" name="" id="" placeholder='******' className="input" />
+                                    </div>
+                                </div>
+                                <div className="field mt-5">
+                                    <label className="label">Confirm Password</label>
+                                    <div className="controls">
+                                        <input type="password" name="" id="" placeholder='******' className="input" />
+                                    </div>
+                                </div>
+                                <div className="field mt-5">
+                                    <button className="button is-success is-fullwidth">Register</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default Register;
